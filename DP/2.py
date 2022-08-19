753e2e31c9a7ee3a4aef4b8135bc9460d98a988f
SH <soso6079@gmail.com>
2022-08-19 14:47:31 +0900

    unsolved 개미전사

--- DP/2.py
+++ DP/2.py
@@ -1,7 +1,11 @@
+# 20분 풀이 베낌
 a = [1,3,1,5]
 
-d = 0
-for i in range(len(a)-1):
-    d += max(a[i],a[i+1])
+d = dict()
+d[0] = a[0]
+d[1] = max(a[1],a[0])
+for i in range(2,len(a)):
+    d[i] = max(d[i-1], d[i-2]+a[i])
+
 
     pass
