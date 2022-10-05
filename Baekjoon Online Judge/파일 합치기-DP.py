043a205cc12d6d953891a7e4ba5b2695fced53ac
soso6079 <soso6079@gmail.com>
2022-10-05 17:29:16 +0900

    파일 합치기 unsolved

---
+++ Baekjoon Online Judge/파일 합치기-DP.py
@@ -0,0 +1,20 @@
+# 파일 합치기 # 2022-10-05
+# https://www.acmicpc.net/problem/11066
+# https://data-make.tistory.com/402
+import sys
+
+t = int(sys.stdin.readline())
+k1 = int(sys.stdin.readline())
+k1_list = list(map(int, sys.stdin.readline().split()))
+
+k2 = int(sys.stdin.readline())
+k2_list = list(map(int, sys.stdin.readline().split()))
+
+d1 = []
+for i in range(k1+1):
+    d1.append([])
+
+for i in k1:
+
+
+print(d1)
